4f844826110a0027ad6e73ba436307ad5df8b39d
kvokka <kvokka@yahoo.com>
2019-04-27 05:33:24 +0100

    Add lesson 10

---
+++ lesson10_1_CountFactors.rb
@@ -0,0 +1,24 @@
+# A positive integer D is a factor of a positive integer N if there exists an integer M such that N = D * M.
+
+# For example, 6 is a factor of 24, because M = 4 satisfies the above condition (24 = 6 * 4).
+
+# Write a function:
+
+# def solution(n)
+
+# that, given a positive integer N, returns the number of its factors.
+
+# For example, given N = 24, the function should return 8, because 24 has 8 factors, namely 1, 2, 3, 4, 6, 8, 12, 24. There are no other factors of 24.
+
+# Write an efficient algorithm for the following assumptions:
+
+# N is an integer within the range [1..2,147,483,647].
+
+def solution(n)
+  sqrt = Math.sqrt(n)
+  correction = sqrt == sqrt.floor ? 1 : 0
+  
+  (1..(sqrt.floor)).inject(0) do |acc, el|
+    n % el == 0 ? acc + 2 : acc
+  end - correction
+end
